5a69e18328e2575a056800e2f7ac6550a12e4348
Nadykryv <nkryvonis@gmail.com>
2025-01-24 14:55:16 +0100

    add database

---
+++ src/database/database.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  constructor(private readonly configService: ConfigService) {}
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    const postgresConfig = this.configService.get('database');
+    return {
+      type: 'postgres',
+      host: postgresConfig.host,
+      port: postgresConfig.port,
+      username: postgresConfig.user,
+      password: postgresConfig.password,
+      database: postgresConfig.database,
+      entities: [User],
+      // migrations
+      synchronize: false,
+    };
+  }
+}
---
+++ src/database/entities/user.entity.ts
@@ -0,0 +1,31 @@
+import { Column, Entity,  PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity } from './base.entity';
+
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', { nullable: true })
+  firstName: string;
+
+  @Column('text', { nullable: true })
+  lastName: string;
+
+  @Column('text', { nullable: false, unique: true })
+  email: string;
+
+  @Column('text', { nullable: false })
+  password: string;
+
+  @Column('text', { nullable: true, default: 'Lviv' })
+  city: string;
+
+  @Column('integer', { nullable: true })
+  age: number;
+
+  @Column({ default: false })
+  isActive: boolean;
+
+}
